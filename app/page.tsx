8609aee7181e8ee2dc642b7bac1ab63986ef7b1b
Батухан Байдыбек <batuhan7sk@gmail.com>
2025-05-29 09:56:52 +0500

    v9

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,11 @@
-'use client';
 
-import { useEffect, useState } from 'react';
-
-interface TelegramWebAppUser {
-    id: number;
-    first_name: string;
-    last_name?: string;
-    username?: string;
-    language_code?: string;
-}
-
-interface TelegramWebAppInitData {
-    user?: TelegramWebAppUser;
-}
-
-interface TelegramWebApp {
-    initDataUnsafe: TelegramWebAppInitData;
-    ready: () => void;
-    sendData: (data: string) => void;
-}
-
-declare global {
-    interface Window {
-        Telegram?: {
-            WebApp: TelegramWebApp;
-        };
-    }
-}
 
 export default function Home() {
-    const [tg, setTg] = useState<TelegramWebApp | null>(null);
+  
 
-    useEffect(() => {
-        if (window.Telegram && window.Telegram.WebApp) {
-            console.log('Telegram WebApp:', window.Telegram.WebApp);
-            setTg(window.Telegram.WebApp);
-            window.Telegram.WebApp.ready();
-        } else {
-            console.log('Telegram WebApp not found');
-        }
-    }, []);
-
-    const user = tg?.initDataUnsafe.user;
-
-    const sendData = () => {
-        if (tg) tg.sendData(JSON.stringify({ action: 'test', message: 'Hello from MiniApp' }));
-    };
-
-    return (
-        <div>
-            <h1>Telegram Mini App</h1>
-            {tg ? (
-                <>
-                    {tg.initDataUnsafe.user ? (
-                        <p>Привет, {tg.initDataUnsafe.user.first_name}</p>
-                    ) : (
-                        <p>Пользователь не найден в initDataUnsafe</p>
-                    )}
-                    <button onClick={sendData}>Отправить данные боту</button>
-                    <div>{user && JSON.stringify(user, null, 2)}</div>
-                </>
-            ) : (
-                <p>Telegram WebApp не загружен</p>
-            )}
-        </div>
-    );
-
+  return (
+    <div>
+      Test
+    </div>
+  );
 }
