d7f586481fc23d73b25aad89e9c40cd72ca1de63
Батухан Байдыбек <batuhan@kdl.olymp>
2025-05-29 08:38:40 +0500

    v2

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,7 @@ import { useEffect } from "react";
 
 
 export default function Home() {
-    useEffect(() => {
 
-        const bot = window.Telegram?.WebApp
-        const user = bot?.initDataUnsafe?.user
-
-        console.log(user);
-        
-
-
-    })
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <button>Test</button>
