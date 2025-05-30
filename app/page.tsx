3f2907e1c1ded054cfe9427f8eaa4c986d1f1e42
Батухан Байдыбек <batuhan7sk@gmail.com>
2025-05-30 15:01:34 +0500

    latest-2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import CheckedUserAccount from '@/lib/checkedUserAccount';
 import loadTelegramWebApp from '@/lib/telegram';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Header from '@/components/header';
 import { useDataStore } from '@/store/store';
@@ -11,50 +11,53 @@ import { useDataStore } from '@/store/store';
 export default function Home() {
     const router = useRouter()
     const [user, setUser] = useState<TelegramUser | null>(null);
-    const { userData ,setUserData } = useDataStore()
+    const { userData, setUserData } = useDataStore()
+
+    const initTelegram = useCallback(async () => {
+        console.log('Запускаю initTelegram...');
+        
+        const tg = await loadTelegramWebApp() /* Инициализация WebApp */
+        const idUser = tg.initDataUnsafe?.user?.id
+
+        if (idUser) {
+
+            const statusResponse = await CheckedUserAccount(idUser) /* Проверка аккаунта */
+
+            if (statusResponse === 403) {
+
+                const message = encodeURIComponent('Пользователь не найден или доступ запрещён');
+                router.replace(`/no-access?message=${message}`);
+
+                return
+            }
+            else if (statusResponse === 500) {
+
+                const message = encodeURIComponent('Ошибка сервера');
+                router.replace(`/no-access?message=${message}`);
+
+                return
+            }
+
+            setUserData(tg.initDataUnsafe.user!)
+            return setUser(tg.initDataUnsafe.user!)
+        }
+        else {
+            const message = encodeURIComponent('Доступ запрещён');
+            router.replace(`/no-access?message=${message}`);
+        }
+    }, [router])
 
 
     useEffect(() => {
+        console.log('Проверка userData:', userData);
+
         if (userData) {
             setUser(userData)
             return
-          }
-
-        async function initTelegram() {
-
-            const tg = await loadTelegramWebApp() /* Инициализация WebApp */
-            const idUser = tg.initDataUnsafe?.user?.id
-
-            if (idUser) {
-
-                const statusResponse = await CheckedUserAccount(idUser) /* Проверка аккаунта */
-
-                if (statusResponse === 403) {
-
-                    const message = encodeURIComponent('Пользователь не найден или доступ запрещён');
-                    router.replace(`/no-access?message=${message}`);
-
-                    return
-                }
-                else if (statusResponse === 500) {
-
-                    const message = encodeURIComponent('Ошибка сервера');
-                    router.replace(`/no-access?message=${message}`);
-
-                    return
-                }
-
-                setUserData(tg.initDataUnsafe.user!)
-                return setUser(tg.initDataUnsafe.user!)
-            }
-            else {
-                const message = encodeURIComponent('Доступ запрещён');
-                router.replace(`/no-access?message=${message}`);
-            }
         }
 
         initTelegram();
-    }, []);
+    }, [userData, initTelegram]);
 
     return (
         <div className='h-screen w-full'>
--- lib/checkedUserAccount.ts
+++ lib/checkedUserAccount.ts
@@ -1,12 +1,15 @@
 export default async function CheckedUserAccount(id: number): Promise<number> {
-    
+
 
     try {
         const response = await fetch(`/api/accounts?id=${id}`)
 
         return response.status
     }
-    catch(e) {
-        return 500
+    catch (e: unknown) {
+        if (e instanceof Error) {
+            console.error('Ошибка:', e.message);
+        }
+        return 500;
     }
 }
